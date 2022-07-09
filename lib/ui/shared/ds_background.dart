b7a13bbfc6dbbeec3c70f4f666b9b9fee3a55805
lucas-goldner <lucas.goldner@googlemail.com>
2022-07-09 16:13:36 +0200

    fix: Remove magic numbers

--- lib/ui/shared/ds_background.dart
+++ lib/ui/shared/ds_background.dart
@@ -33,6 +33,7 @@ class DSBackgroundPainter extends CustomPainter {
   void _paintLinesBackground(Canvas canvas, Size size) {
     // ScaleFactor is used to scale the background to the whole screen.
     double scaleFactor = 0.1;
+    int two = 2;
     final paint = Paint()
       ..color = ChatToPicColors.dsGreyBackground
       ..strokeWidth = 1;
@@ -41,31 +42,39 @@ class DSBackgroundPainter extends CustomPainter {
       final postion1 = Offset(0, index);
       final postion2 = Offset(size.width, index);
       paint.color =
-          index % 2 == 0 ? ChatToPicColors.dsGreyBackground : Colors.white;
+          index % two == 0 ? ChatToPicColors.dsGreyBackground : Colors.white;
 
       canvas.drawLine(postion1, postion2, paint);
     }
   }
 
   void _paintSquares(Canvas canvas, Size size) {
-    // ScaleFactor is used to scale the background to the whole screen.
     double scaleFactor = 0.1;
-    for (int index = 0; index <= size.height / scaleFactor; index += 150) {
-      _paintSquaresRow(canvas, size, index);
+    int squareSize = 50;
+    int squarePosition2Offset = 100;
+    int squareHeight = squareSize + squarePosition2Offset;
+
+    for (int index = 0;
+        index <= size.height / scaleFactor;
+        index += squareHeight) {
+      _paintSquaresRow(canvas, size, index, squareSize);
     }
   }
 
-  void _paintSquaresRow(Canvas canvas, Size size, int squareStartingPoint) {
+  void _paintSquaresRow(
+    Canvas canvas,
+    Size size,
+    int squareStartingPoint,
+    int squareSize,
+  ) {
     final paint = Paint()
       ..color = ChatToPicColors.dsSquareOutlineColor
       ..strokeWidth = 1
       ..style = PaintingStyle.stroke;
 
-    int squareSize = 50;
     int postion1Iterator = 0;
-
-    // ScaleFactor is used to scale the background to the whole screen.
     double scaleFactor = 0.1;
+    int distanceToNextSquareMultiplier = 3;
     for (int index = squareSize;
         index <= size.width / scaleFactor;
         index += squareSize) {
@@ -75,7 +84,7 @@ class DSBackgroundPainter extends CustomPainter {
 
       final rect = Rect.fromPoints(postion1, postion2);
       canvas.drawRect(rect, paint);
-      postion1Iterator += squareSize * 3;
+      postion1Iterator += squareSize * distanceToNextSquareMultiplier;
     }
   }
 }
