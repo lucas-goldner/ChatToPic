ad9a84745bab1fe10103890175beac877d52bb80
lucas-goldner <lucas.goldner@googlemail.com>
2022-07-17 16:48:53 +0200

    feat: Adjust color codes

--- lib/ui/constants/chat_to_pic_colors.dart
+++ lib/ui/constants/chat_to_pic_colors.dart
@@ -7,20 +7,20 @@ class ChatToPicColors {
   static const dsModalBackgroundColor = Color(0xFF484848);
 
   // Colors to choose from.
-  static const dsGreyColor = Color(0xFF4A809C);
-  static const dsBrownColor = Color(0xFFAC4F26);
-  static const dsRedColor = Color(0xFFEE3233);
-  static const dsCottonCandyColor = Color(0xFFC78DBF);
-  static const dsOrangeColor = Color(0xFFF28E34);
-  static const dsYellowColor = Color(0xFFF0EA26);
-  static const dsLimeGreenColor = Color(0xFF9DCD46);
-  static const dsGreenColor = Color(0xFF56B847);
-  static const dsDarkGreenColor = Color(0xFF00A24C);
-  static const dsEmeraldColor = Color(0xFF51BD8C);
-  static const dsSkyBlueColor = Color(0xFF01B0E7);
-  static const dsBlueColor = Color(0xFF01B0E7);
-  static const dsDarkBlueColor = Color(0xFF283770);
-  static const dsPurpleColor = Color(0xFF684B9D);
-  static const dsLightPurpleColor = Color(0xFF9051A0);
-  static const dsPinkColor = Color(0xFFEF328E);
+  static const dsGreyColor = Color(0xFF61829A);
+  static const dsBrownColor = Color(0xFFBA4900);
+  static const dsRedColor = Color(0xFFFB0018);
+  static const dsCottonCandyColor = Color(0xFFFB8AFB);
+  static const dsOrangeColor = Color(0xFFFB9200);
+  static const dsYellowColor = Color(0xFFF3E300);
+  static const dsLimeGreenColor = Color(0xFFAAFB00);
+  static const dsGreenColor = Color(0xFF00FB00);
+  static const dsDarkGreenColor = Color(0xFF00A238);
+  static const dsEmeraldColor = Color(0xFF49DB8A);
+  static const dsSkyBlueColor = Color(0xFF30BAF3);
+  static const dsBlueColor = Color(0xFF0059F3);
+  static const dsDarkBlueColor = Color(0xFF000092);
+  static const dsPurpleColor = Color(0xFF8A00D3);
+  static const dsLightPurpleColor = Color(0xFFD300EB);
+  static const dsPinkColor = Color(0xFFFB0092);
 }
